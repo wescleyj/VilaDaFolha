2f57a5c129a0c32592ab6363a9919a728bc55c43
Giovanni Estevam <gioestevam04@gmail.com>
2025-07-26 23:00:51 -0300

    feat: Alterando os parâmetros do construtor Ninja

--- src/principal/model/Ninja.java
+++ src/principal/model/Ninja.java
@@ -2,30 +2,14 @@ package principal.model;
 
 public class Ninja extends Morador{
     private TipoNinja tipo;
-    private String cla;
-    private String time;
 
-    public Ninja(String nome, int idade, Sexo sexo, Status status, TipoNinja tipo, String cla, String time) {
+    public Ninja(String nome, int idade, Sexo sexo, Status status, TipoNinja tipo) {
         super(nome, idade, sexo, status);
         this.tipo = tipo;
-        this.cla = cla;
-        this.time = time;
     }
 
     public TipoNinja getTipo() {
         return tipo;
-    }
-
-    public String getCla() {
-        return cla;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
     }
 
     public void setTipo(TipoNinja tipo) {
